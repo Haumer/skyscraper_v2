eeb3dc14a9a2e99622db0e05e632d3af9d319e72
Alexander Haumer <alexander.haumer@me.com>
2019-08-05 19:25:10 +0100

    added fields to scraper: card_class, title_class, salary_class, location_class, link_class, company_class

---
+++ db/migrate/20190805181644_add_fields_to_scraper.rb
@@ -0,0 +1,10 @@
+class AddFieldsToScraper < ActiveRecord::Migration[5.1]
+  def change
+    add_column :scrapers, :card_class, :string
+    add_column :scrapers, :title_class, :string
+    add_column :scrapers, :salary_class, :string
+    add_column :scrapers, :location_class, :string
+    add_column :scrapers, :link_class, :string
+    add_column :scrapers, :company_class, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190805180501) do
+ActiveRecord::Schema.define(version: 20190805181644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,12 @@ ActiveRecord::Schema.define(version: 20190805180501) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "website_id"
+    t.string "card_class"
+    t.string "title_class"
+    t.string "salary_class"
+    t.string "location_class"
+    t.string "link_class"
+    t.string "company_class"
     t.index ["website_id"], name: "index_scrapers_on_website_id"
   end
 
