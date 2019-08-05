067fff7a53568f0a75f0b1fafc3b9b6ae2e1821a
Alexander Haumer <alexander.haumer@me.com>
2019-08-05 18:53:13 +0100

    added user to the searches table, has_many and belongs_to

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,5 +1,6 @@
 class Search < ApplicationRecord
   has_many :jobs
+  belongs_to :user
   validates :keyword, presence: true
   validates :salary, presence: true
   validates :location, presence: true
