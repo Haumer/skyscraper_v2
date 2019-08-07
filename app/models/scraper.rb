9a76e133bd25ded87e9596370cbc3a515feb6ad2
Alexander Haumer <alexander.haumer@me.com>
2019-08-07 09:12:32 +0100

    removing interpolation for the scraper.crawl model

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -14,16 +14,16 @@ class Scraper < ApplicationRecord
   def crawl(keyword, location)
     build_url(keyword, location).each do |url|
       page = Nokogiri::HTML(open(url))
-      page.search("#{self.card_class}").each do |result_card|
-        if result_card.search("#{self.title_class}").text.strip.downcase.include?(keyword)
-          title = result_card.search("#{self.title_class}").text.strip
-          link = self.website.name + result_card.search("#{self.link_class}").first['href']
-          location = result_card.search("#{self.location_class}").text.strip
-          company = result_card.search("#{self.company_class}").text.strip
-          if result_card.search("#{self.salary_class}").text.strip.nil? || result_card.search("#{self.salary_class}").text.strip.empty?
+      page.search(self.card_class).each do |result_card|
+        if result_card.search(self.title_class).text.strip.downcase.include?(keyword)
+          title = result_card.search(self.title_class).text.strip
+          link = self.website.name + result_card.search(self.link_class).first['href']
+          location = result_card.search(self.location_class).text.strip
+          company = result_card.search(self.company_class).text.strip
+          if result_card.search(self.salary_class).text.strip.nil? || result_card.search(self.salary_class).text.strip.empty?
             salary = nil
           else
-            salary = result_card.search("#{self.salary_class}").text.strip
+            salary = result_card.search(self.salary_class).text.strip
           end
           website = self.website.name
           Job.create!(
