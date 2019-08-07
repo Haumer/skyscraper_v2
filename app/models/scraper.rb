8d880c2a227d5e624128e963eeaa7384dc4ce05c
Alexander Haumer <alexander.haumer@me.com>
2019-08-07 02:10:12 +0100

    scraper.crawl method runs

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -3,34 +3,38 @@ class Scraper < ApplicationRecord
 
   belongs_to :website
 
-  def crawl(**args)
-    @@url = "https://www.cv-library.co.uk/search-jobs?distance=15&fp=1&geo=london&offset=0&posted=28&q=ruby&salarymax=&salarymin=&salarytype=annum&search=1&tempperm=Any"
-    page = Nokogiri::HTML(open(@@url))
-    page.search("#{args[:card_class]}").each do |result_card|
-      if result_card.search("#{args[:title_class]}").text.strip.downcase.include?("#{args[:keyword]}")
-        title = result_card.search("#{args[:title_class]}").text.strip
-        link = "#{args[:name]}" + result_card.search("#{args[:link_class]}").first['href']
-        location = result_card.search("#{args[:location_class]}").text.strip
-        company = result_card.search("#{args[:company_class]}").text.strip
-        if result_card.search("#{args[:salary_class]}").text.strip.nil? || result_card.search("#{args[:salary_class]}").text.strip.empty?
-          salary = nil
-        else
-          salary = result_card.search("#{args[:salary_class]}").text.strip
+  def crawl(keyword, location)
+    build_url(keyword, location).each do |url|
+      page = Nokogiri::HTML(open(url))
+      page.search("#{self.card_class}").each do |result_card|
+        if result_card.search("#{self.title_class}").text.strip.downcase.include?(keyword)
+          title = result_card.search("#{self.title_class}").text.strip
+          link = self.website.name + result_card.search("#{self.link_class}").first['href']
+          location = result_card.search("#{self.location_class}").text.strip
+          company = result_card.search("#{self.company_class}").text.strip
+          if result_card.search("#{self.salary_class}").text.strip.nil? || result_card.search("#{self.salary_class}").text.strip.empty?
+            salary = nil
+          else
+            salary = result_card.search("#{self.salary_class}").text.strip
+          end
+          website = self.website.name
+          Job.create!(
+            title: title,
+            location: location,
+            job_website: website,
+            salary: salary,
+            company: company,
+            link: link,
+          )
         end
-        website = "#{args[:name]}"
-        Job.create!(
-          title: title,
-          location: location,
-          job_website: website,
-          salary: salary,
-          company: company,
-          link: link,
-        )
       end
     end
   end
 
-  def build_url()
-
+  def build_url(keyword, location)
+    url = self.scrape_url.gsub("KEYWORD", keyword).gsub("LOCATION", location)
+    (0...self.nr_pages).map do |count|
+      url.gsub("COUNTER", "#{self.counter_interval * count}")
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,17 +4,17 @@ websites = %w( www.cv-library.co.uk www.jobstoday.co.uk www.indeed.co.uk www.tot
 
 websites.each do |website|
   name = website.split(".")[1]
-  Website.create!(url: website, name: name)
+  Website.create!(base_url: website, name: name)
 end
 
-# Scraper.create!(
-#   card_class: ".job-search-description",
-#   title_class: "#js-jobtitle-details",
-#   link_class: ".jobtitle-divider a",
-#   location_class: "#js-loc-details",
-#   company_class: ".agency-link-mobile",
-#   salary_class: "#js-salary-details",
-#   website: Website.first,
-#   nr_pages: 3,
-#   scrape_url: "https://www.cv-library.co.uk/search-jobs?distance=15&fp=1&geo=LOCATION&offset=COUNTER&posted=28&q=KEYWORD&salarymax=&salarymin=&salarytype=annum&search=1&tempperm=Any"
-# )
+Scraper.create!(
+  card_class: ".job-search-description",
+  title_class: "#js-jobtitle-details",
+  link_class: ".jobtitle-divider a",
+  location_class: "#js-loc-details",
+  company_class: ".agency-link-mobile",
+  salary_class: "#js-salary-details",
+  website: Website.first,
+  nr_pages: 3,
+  scrape_url: "https://www.cv-library.co.uk/search-jobs?distance=15&fp=1&geo=LOCATION&offset=COUNTER&posted=28&q=KEYWORD&salarymax=&salarymin=&salarytype=annum&search=1&tempperm=Any"
+)
