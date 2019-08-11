2df1b5a2d40db3dc285224ddcf8c27e6c6bca905
Alexander Haumer <alexander.haumer@me.com>
2019-08-11 20:18:33 +0100

    added show and index for searches, created routes and controller methods

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,11 +1,20 @@
 class SearchesController < ApplicationController
-
+  include ActionController::Live
   def new
     @search = Search.new
   end
 
+  def index
+    @search = Search.all
+  end
+
   def create
     @search = Search.new(user: current_user, keyword: search_params[:keyword])
+  end
+
+  def show
+    @search = Search.find(params[:id])
+    @jobs = @search.jobs
   end
 
   private
