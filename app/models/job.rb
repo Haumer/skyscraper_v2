cb9d804e29f99d34086e22eef51c16d17f98347e
alexander <alexander.haumer@me.com>
2020-05-30 23:21:38 +0100

    initial styling for the job card content

--- app/models/job.rb
+++ app/models/job.rb
@@ -9,4 +9,21 @@ class Job < ApplicationRecord
   validates :job_website, presence: true
   validates :quality, presence: true
 
+  def width
+    'grid-item--width2' if quality > 2
+  end
+
+  def height
+    'grid-item--height2' if quality > 3
+  end
+
+  def title_length
+    if quality > 3
+      999
+    elsif quality > 2
+      70
+    else
+      30
+    end
+  end
 end
