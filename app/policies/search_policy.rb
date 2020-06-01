0445c1816f95086c631cd172d6b38dd67cbd8e01
alexander <alexander.haumer@me.com>
2020-06-01 21:31:42 +0100

    search policy

--- app/policies/search_policy.rb
+++ app/policies/search_policy.rb
@@ -1,8 +1,16 @@
 class SearchPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.all
+      if user.active_search || user.admin
+        scope.all
+      else
+        scope.first(5)
+      end
     end
+  end
+
+  def index?
+    false
   end
 
   def create?
@@ -10,6 +18,16 @@ class SearchPolicy < ApplicationPolicy
   end
 
   def show?
-    true
+    user_is_owner_or_admin?
+  end
+
+  private
+
+  def user_is_owner_or_admin?
+    record.user == user || user.admin
+  end
+
+  def user_can_search_or_admin?
+    user.active_search || user.admin
   end
 end
