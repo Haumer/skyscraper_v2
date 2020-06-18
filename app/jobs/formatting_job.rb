fbcd3bb2b9a58c6180caeca89e6485775251456d
alexander <alexander.haumer@me.com>
2020-06-18 23:41:55 +0100

    cleaned up formatting

--- app/jobs/formatting_job.rb
+++ app/jobs/formatting_job.rb
@@ -5,36 +5,50 @@ class FormattingJob < ApplicationJob
   def perform(id)
     @search = Search.find(id)
     @search.jobs.each do |job|
-      job.title = job.title.gsub(/\s+/, " ")
-      job.location = job.location.gsub(/\s+/, " ")
-      job.description = job.location.gsub(/\s+/, " ")
-      if job.company[0..(job.company.length/2 - 1)] == job.company[job.company.length/2..-1]
-        job.company = job.company[0..job.company.length/2 - 1]
-      end
-      if job.salary.gsub(/Â/,"").scan(/(\d{1,3}[,\.]?\d{0,3})/).flatten.length < 1
-        job.update!(salary: "unspecified")
-      else
-        job.update!(salary: job.salary.scan(/(\d{1,3}[,\.]?\d{0,3})/).join("-"))
-      end
-
-      salaries = job.salary.scan(/(\d{1,3}[,\.]?\d{0,3})/).flatten
-      unless salaries.length <= 1
-        salaries = salaries.flatten.map { |salary| salary.scan(/\d/).join.to_i }
-        p salaries
-         if salaries[0] < salaries[1]
-           job.lower_salary = salaries[0].to_i
-           job.upper_salary = salaries[1].to_i
-         else
-           job.lower_salary = salaries[0].to_i
-           job.upper_salary = salaries[1].to_i
-        end
-        job.save
-      end
+      job = remove_space_chars(job)
+      job = remove_duplicate_name(job)
+      job = salary_digits(job)
+      job = salary_bounds(salaries)
+      job.save
     end
   end
 
   # TODO: change update into one db call
   private
+
+  def remove_space_chars(job)
+    job.title = job.title.gsub(/\s+/, " ")
+    job.location = job.location.gsub(/\s+/, " ")
+    job.description = job.location.gsub(/\s+/, " ")
+    job
+  end
+
+  def remove_duplicate_name(job)
+    double? = job.company[0..(job.company.length/2 - 1)] == job.company[job.company.length/2..-1]
+    job.company = job.company[0..job.company.length/2 - 1] if double?
+    job
+  end
+
+  def salary_digits(job)
+    salary_present = job.salary.scan(/(\d{1,3}[,\.]?\d{0,3})/).flatten.length > 1
+    job.salary = salary_present ? job.salary.scan(/(\d{1,3}[,\.]?\d{0,3})/).join("-") : "unspecified"
+    job
+  end
+
+  def salary_bounds(job)
+    salaries = job.salary.scan(/(\d{1,3}[,\.]?\d{0,3})/).flatten
+    unless salaries.length <= 1
+      salaries = salaries.flatten.map { |salary| salary.scan(/\d/).join.to_i }
+      if salaries[0] < salaries[1]
+        job.lower_salary = salaries[0].to_i
+        job.upper_salary = salaries[1].to_i
+      else
+        job.lower_salary = salaries[0].to_i
+        job.upper_salary = salaries[1].to_i
+      end
+    end
+    job
+  end
 
   def update_status_formatted
     @search.update(status_scraped: true, status_message: "finished formatting")
